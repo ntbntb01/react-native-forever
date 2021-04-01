997fa50f028f9d558f2649da473bc7afea221202
ntbntb01 <64126111+ntbntb01@users.noreply.github.com>
2021-04-01 20:26:06 +0700

    demo

--- Abc.js
+++ Abc.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native'
 export default function Abc() {
     return (
         <View>
-            <Text></Text>
+            <Text>asasas</Text>
         </View>
     )
 }
